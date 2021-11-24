07be441ef3c3e44bfd1fef0aa54223b882f0c5b1
PallaDeepika <palladeepika245@gmail.com>
2021-11-24 12:06:19 +0530

    intial commit

---
+++ hooksapp/hooksapp/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import ClickCounter from './components/higherorderComponents/ClickCounter';
+import HoverCounter from './components/higherorderComponents/HoverCounter';
+
+
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+     <ClickCounter/>
+     <HoverCounter/>
+    </div>
+
+  );
+}
+
+export default App;
